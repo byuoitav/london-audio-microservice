0c4fd77056f8b36833ecb4eaede905e3a689245b
Jesse Millar <hellojessemillar@gmail.com>
2016-11-23 10:56:05 -0700

    Vendoring dependencies and moving to Echo v3

--- handlers/rawHandler.go
+++ handlers/rawHandler.go
@@ -13,16 +13,20 @@ func Raw(context echo.Context) error {
 
 	bindError := context.Bind(&command)
 	if bindError != nil {
-		return jsonresp.New(context, http.StatusBadRequest, "Could not read command body: "+bindError.Error())
+		jsonresp.New(context.Response(), http.StatusBadRequest, "Could not read command body: "+bindError.Error())
+		return nil
 	}
 
 	commandError := londondi.HandleRawCommand(command)
 	if commandError != nil {
-		return jsonresp.New(context, http.StatusBadRequest, commandError.Error())
+		jsonresp.New(context.Response(), http.StatusBadRequest, commandError.Error())
+		return nil
 	}
+
 	return commandError
 }
 
 func RawInfo(context echo.Context) error {
-	return jsonresp.New(context, http.StatusBadRequest, "Send a POST request to the /raw endpoint with a body including Command string")
+	jsonresp.New(context.Response(), http.StatusBadRequest, "Send a POST request to the /raw endpoint with a body including Command string")
+	return nil
 }
--- server.go
+++ server.go
@@ -2,35 +2,41 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/byuoitav/hateoas"
 	"github.com/byuoitav/london-audio-microservice/handlers"
 	"github.com/byuoitav/wso2jwt"
 	"github.com/jessemillar/health"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/fasthttp"
 	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-	port := ":8009"
-	router := echo.New()
-	router.Pre(middleware.RemoveTrailingSlash())
-	router.Use(middleware.CORS())
-
 	err := hateoas.Load("https://raw.githubusercontent.com/byuoitav/london-audio-microservice/master/swagger.json")
 	if err != nil {
 		log.Fatalln("Could not load swagger.json file. Error: " + err.Error())
 	}
 
-	router.Get("/", hateoas.RootResponse)
-	router.Get("/health", health.Check)
-	router.Get("/raw", handlers.RawInfo, wso2jwt.ValidateJWT())
+	port := ":8009"
+	router := echo.New()
+	router.Pre(middleware.RemoveTrailingSlash())
+	router.Use(middleware.CORS())
 
-	router.Post("/raw", handlers.Raw, wso2jwt.ValidateJWT())
+	// Use the `secure` routing group to require authentication
+	secure := router.Group("", echo.WrapMiddleware(wso2jwt.ValidateJWT))
 
-	log.Println("The London Audio microservice is listening on " + port)
-	server := fasthttp.New(port)
-	server.ReadBufferSize = 1024 * 10 // Needed to interface properly with WSO2
-	router.Run(server)
+	router.GET("/", echo.WrapHandler(http.HandlerFunc(hateoas.RootResponse)))
+	router.GET("/health", echo.WrapHandler(http.HandlerFunc(health.Check)))
+
+	secure.GET("/raw", handlers.RawInfo)
+
+	secure.POST("/raw", handlers.Raw)
+
+	server := http.Server{
+		Addr:           port,
+		MaxHeaderBytes: 1024 * 10,
+	}
+
+	router.StartServer(&server)
 }
