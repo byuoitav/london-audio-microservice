d32fa10a112f69bcbb5a05fb2c5b197e4e4fdfa2
thailandmatt <31248374+thailandmatt@users.noreply.github.com>
2018-11-05 14:00:30 -0700

    remove echo from vendor and auth changes

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 
-	"github.com/byuoitav/authmiddleware"
 	"github.com/byuoitav/hateoas"
 	"github.com/byuoitav/london-audio-microservice/handlers"
 	"github.com/jessemillar/health"
@@ -18,23 +17,23 @@ func main() {
 	router.Use(middleware.CORS())
 
 	// Use the `secure` routing group to require authentication
-	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
+	//secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
 
 	router.GET("/", echo.WrapHandler(http.HandlerFunc(hateoas.RootResponse)))
 	router.GET("/health", echo.WrapHandler(http.HandlerFunc(health.Check)))
 	router.GET("/status", echo.WrapHandler(http.HandlerFunc(health.Check)))
 
-	secure.GET("/raw", handlers.RawInfo)
+	router.GET("/raw", handlers.RawInfo)
 
 	//functionality
-	secure.POST("/raw", handlers.Raw)
-	secure.GET("/:address/:input/volume/mute", handlers.Mute)
-	secure.GET("/:address/:input/volume/unmute", handlers.UnMute)
-	secure.GET("/:address/:input/volume/set/:level", handlers.SetVolume)
+	router.POST("/raw", handlers.Raw)
+	router.GET("/:address/:input/volume/mute", handlers.Mute)
+	router.GET("/:address/:input/volume/unmute", handlers.UnMute)
+	router.GET("/:address/:input/volume/set/:level", handlers.SetVolume)
 
 	//status
-	secure.GET("/:address/:input/volume/level", handlers.GetVolume)
-	secure.GET("/:address/:input/mute/status", handlers.GetMute)
+	router.GET("/:address/:input/volume/level", handlers.GetVolume)
+	router.GET("/:address/:input/mute/status", handlers.GetMute)
 
 	server := http.Server{
 		Addr:           port,
