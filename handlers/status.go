b068f66922e145911e46d75849d88aed78f634f9
jpw547 <jwerner547@gmail.com>
2018-12-18 12:23:39 -0700

    re-working the authentication

--- handlers/status.go
+++ handlers/status.go
@@ -4,20 +4,11 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/byuoitav/common/log"
-	"github.com/byuoitav/common/v2/auth"
 	di "github.com/byuoitav/london-audio-microservice/londondi"
 	"github.com/labstack/echo"
 )
 
 func GetMute(context echo.Context) error {
-	if ok, err := auth.CheckAuthForLocalEndpoints(context, "read-state"); !ok {
-		if err != nil {
-			log.L.Warnf("Problem getting auth: %v", err.Error())
-		}
-		return context.String(http.StatusUnauthorized, "unauthorized")
-	}
-
 	status, err := di.GetMute(context.Param("address"), context.Param("input"))
 	if err != nil {
 		msg := fmt.Sprintf("Error: %s", err.Error())
@@ -29,13 +20,6 @@ func GetMute(context echo.Context) error {
 }
 
 func GetVolume(context echo.Context) error {
-	if ok, err := auth.CheckAuthForLocalEndpoints(context, "read-state"); !ok {
-		if err != nil {
-			log.L.Warnf("Problem getting auth: %v", err.Error())
-		}
-		return context.String(http.StatusUnauthorized, "unauthorized")
-	}
-
 	status, err := di.GetVolume(context.Param("address"), context.Param("input"))
 	if err != nil {
 		msg := fmt.Sprintf("Error: %s", err.Error())
